29313b4c7f10fee0403205605348bfe3e88fdce4
Herman Sutanto <h.sutanto@gmail.com>
2026-08-24 09:33:50 +0700

    fix: align Android MainActivity package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.fastclean.attendance_face_prototype"
+    namespace = "com.fastclean.attendance_face"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.fastclean.attendance_face_prototype"
+        applicationId = "com.fastclean.attendance_face"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
